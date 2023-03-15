e4f3e39ab9a4768244ad616fc7cae0dc142e2321
Madhura <cmadhura22j2018@gmail.com>
2023-03-15 07:38:41 +0530

    committed on 15th March 7:40 am

--- src/test/java/rahulshettyacademy/RTtests/SubmitShoeOrders.java
+++ src/test/java/rahulshettyacademy/RTtests/SubmitShoeOrders.java
@@ -8,28 +8,30 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import rahulshettyacademy.RTpageobject.RTCheckout;
 import rahulshettyacademy.RTpageobject.RTLandingPage;
 import rahulshettyacademy.RTpageobject.RTProductCatalogue;
+import rahulshettyacademy.testcomponents.BaseTest;
 
-public class SubmitShoeOrders {
+public class SubmitShoeOrders extends BaseTest{
 
-	public static void main(String[] args) throws InterruptedException {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver(); 
+	@Test
+	public void submitShoeOrder()
+	{
 		String email="cmadhura22j2018@gmail.com";
 		String password="MyBata@123";
 		//String[][][] itemsNeeded= {{{"5086343", "DARKGREY", "6"},{"5592343","PEACH","4"},{"5399084","TURQBLUE","5"}}};
-		driver.manage().window().maximize();
-		// Implicit wait
-		driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);		
-		// Login
-		RTLandingPage landingPage=new RTLandingPage(driver);
-		landingPage.goTo();
+		// launchApplication() will point to RTLandingPage class so it should return landingPage object
+		launchApplication(); // we are calling the method of parent class directly
+		
+		// loginApplication() is written in BaseTest.java which is parent class
+		// loginApplication() points to ProductCatalogue() page
 		RTProductCatalogue productCatalogue=landingPage.loginApplication(email,password);
-		productCatalogue.commonObjectCalling(driver, id, colour, size);
+		productCatalogue.commonObjectCalling(driver, "5086343", "DARKGREY", "6");
+		//productCatalogue.commonObjectCalling(driver, id, colour, size);
 		RTCheckout checkOut=productCatalogue.goToCartPage();
 		checkOut.commonObjectCalling();
 		driver.close();
